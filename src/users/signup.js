1e1f88a67ac070b5346a0b5a8d765eae591d722e
Chris Le <le.chris@northeastern.edu>
2023-12-04 13:59:19 -0500

    a6 Mongo fa23

---
+++ src/users/signup.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as client from "./client";
+function Signup() {
+  const [error, setError] = useState("");
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const signup = async () => {
+    try {
+      await client.signup(credentials);
+      navigate("/Kanbas/account");
+    } catch (err) {
+        setError(err.response.data.message);
+    }
+  };
+  return (
+    <div className="content">
+      <h1>Signup</h1>
+      {error && <div>{error}</div>}
+      <input
+        value={credentials.username}
+        onChange={(e) =>
+          setCredentials({
+            ...credentials,
+            username: e.target.value,
+          })
+        }
+        placeholder="username"
+      />
+      <input
+        value={credentials.password}
+        onChange={(e) =>
+          setCredentials({
+            ...credentials,
+            password: e.target.value,
+          })
+        }
+        placeholder="password"
+      />
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
+}
+export default Signup;
