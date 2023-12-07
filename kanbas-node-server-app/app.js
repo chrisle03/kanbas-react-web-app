f737814d0c45409923f9b8ff17595f17d6835a7d
Chris Le <le.chris@northeastern.edu>
2023-12-07 01:31:59 -0500

    typo

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from 'express'
 import Hello from "./hello.js"
-import Lab5 from "./lab5a.js";
+import Lab5 from "./lab5.js";
 import cors from 'cors'
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
 const app = express()
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
