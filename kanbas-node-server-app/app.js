a8729861bcc488a0c1c0f90484ecfc4b8c14a846
Chris Le <le.chris@northeastern.edu>
2023-12-07 01:10:58 -0500

    mistypo

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from 'cors'
@@ -7,7 +7,7 @@ import ModuleRoutes from './modules/routes.js';
 import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
 const app = express()
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
